8887fb888a078d41cfce7e578ea776d7efedb452
sbaer <steve@mcneel.com>
2011-05-18 11:41:46 -0700

    adding support for more than just colors

--- cloudsettings.py
+++ cloudsettings.py
@@ -4,8 +4,7 @@ hosted in a test account I set up on cloudant.com
 """
 import sys, json, urllib, urllib2, hashlib
 import rhinoscriptsyntax as rs
-import System
-import Rhino
+import System, Rhino
 
 couchdb_url = "https://stevebaer.cloudant.com/rhinotest"
 
@@ -15,22 +14,24 @@ def cloudrestore(id):
     Parameters:
         id: name of the document store in the database
     """
-    url = couchdb_url + "/" + id
-    f = urllib.urlopen(url)
+    f = urllib.urlopen(couchdb_url + "/" + id)
     data = json.load(f)
     f.close()
     if data.has_key("error") or not data.has_key("AppearanceSettings"):
         return data
-    def restorecolor(name, color):
+    def restoresetting(name, setting):
         try:
             s = "Rhino.ApplicationSettings.AppearanceSettings." + name
-            s += "=System.Drawing.ColorTranslator.FromHtml(\"" + color + "\")"
+            if name.endswith("Color"):
+                s += "=System.Drawing.ColorTranslator.FromHtml(\"" + setting + "\")"
+            else:
+                if type(setting) is str: s += "='" + setting + "'"
+                else: s+= "=" + str(setting)
             exec(s)
         except:
             print sys.exc_info()
-    subdict = data["AppearanceSettings"]
-    for k, v in subdict.items():
-        restorecolor(k,v)
+    for k, v in data["AppearanceSettings"].items():
+        restoresetting(k,v)
     rs.Redraw()
 
 	
@@ -69,10 +70,7 @@ def cloudsave(id, password=None):
     if hashed_password:
         data["password_hash"] = hashed_password
     subdict = {}
-    if data.has_key("AppearanceSettings"):
-        subdict = data["AppearanceSettings"]
-    else:
-        data["AppearanceSettings"] = subdict
+    data["AppearanceSettings"] = subdict
 
     def addcolor(dict, name):
         s = "Rhino.ApplicationSettings.AppearanceSettings." + name
@@ -82,6 +80,8 @@ def cloudsave(id, password=None):
     addcolor(subdict, "CommandPromptHypertextColor")
     addcolor(subdict, "CommandPromptTextColor")
     addcolor(subdict, "CrosshairColor")
+    addcolor(subdict, "CurrentLayerBackgroundColor")
+    addcolor(subdict, "DefaultObjectColor")
     addcolor(subdict, "FeedbackColor")
     addcolor(subdict, "FrameBackgroundColor")
     addcolor(subdict, "LockedObjectColor")
@@ -92,6 +92,9 @@ def cloudsave(id, password=None):
     addcolor(subdict, "WorldCoordIconXAxisColor")
     addcolor(subdict, "WorldCoordIconYAxisColor")
     addcolor(subdict, "WorldCoordIconZAxisColor")
+    subdict["DefaultFontFaceName"] = Rhino.ApplicationSettings.AppearanceSettings.DefaultFontFaceName
+    subdict["ShowCrosshairs"] = Rhino.ApplicationSettings.AppearanceSettings.ShowCrosshairs
+    subdict["ShowFullPathInTitleBar"] = Rhino.ApplicationSettings.AppearanceSettings.ShowFullPathInTitleBar
     
     j = json.dumps(data)
     req = urllib2.Request(couchdb_url, j, {"content-type":"application/json"})
@@ -99,7 +102,7 @@ def cloudsave(id, password=None):
     response = stream.read()
     stream.close()
     return response
-    
+
 
 def getinput():
     gs = Rhino.Input.Custom.GetString()
@@ -107,8 +110,7 @@ def getinput():
     direction = Rhino.Input.Custom.OptionToggle(True, "SaveSettingsToCloud", "GetSettingsFromCloud")
     gs.AddOptionToggle("direction", direction)
     while( True ):
-        if gs.Get()==Rhino.Input.GetResult.Option:
-            continue
+        if gs.Get()==Rhino.Input.GetResult.Option: continue
         break
     if gs.CommandResult()!=Rhino.Commands.Result.Success:
         return
